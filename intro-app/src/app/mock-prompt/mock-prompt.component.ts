eb117b969a92d1e92b031c1684162e001c9ea480
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-02 19:34:45 -0500

    removed page transition logic and did general clean-up

--- intro-app/src/app/mock-prompt/mock-prompt.component.ts
+++ intro-app/src/app/mock-prompt/mock-prompt.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mock-prompt.component.css']
 })
 export class MockPromptComponent implements OnInit {
-  currentStage: number = 0;
+  
 
   constructor() {
    }
@@ -14,17 +14,6 @@ export class MockPromptComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  stageManager(isNext: boolean){
-    console.log(isNext);
-    //temp to prevent having to refresh
-    if(isNext){
-      if(this.currentStage === 5) this.currentStage = 0;
-      else this.currentStage++;
-    }
-    //temp to prevent having to refresh
-    else {
-      if(this.currentStage > 0) this.currentStage--;
-    }
-  }
+  
 
 }
