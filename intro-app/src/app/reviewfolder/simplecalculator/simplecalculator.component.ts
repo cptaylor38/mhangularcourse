f954543b78350ebcf829080fbbc0fb425868a1c4
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-17 18:56:43 -0500

    break from algos and working on minor calc component

--- intro-app/src/app/reviewfolder/simplecalculator/simplecalculator.component.ts
+++ intro-app/src/app/reviewfolder/simplecalculator/simplecalculator.component.ts
@@ -9,9 +9,48 @@ export class SimplecalculatorComponent implements OnInit {
   operands: string[] = ['C', '+/-', '%', '/', 'x', '-', '+', '='];
   nums: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   value: number = 0;
+  chosenOperand: string = null;
+  chosenNum: number = null;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setNum(number: number){
+    this.chosenNum = number;
+    if(this.chosenNum && this.chosenOperand){
+      this.calculate(number);
+    }
+    else{
+      this.value = number;
+    }
+  }
+
+  setOperand(operand: string){
+    switch(operand){
+      case 'C':
+        this.value = 0;
+        this.chosenNum = null;
+        this.chosenOperand = null;
+        break;
+      case '+/-':
+        if(this.value !== 0){
+          this.value = this.value * -1
+        }
+        break;
+      case '%':
+        if(this.value !== 0){
+          this.value = this.value / 100;
+        }
+        break;
+      default:
+        this.chosenOperand = operand;
+        break;
+    }
+  }
+
+  calculate(num: number){
+    
   }
 
 }
