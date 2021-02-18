4855b6560c7c842ce4161d6f967328a6d190c85b
Corbin Taylor <cptaylor38@yahoo.com>
2021-02-17 20:22:31 -0500

    debugging number event handler

--- intro-app/src/app/reviewfolder/simplecalculator/simplecalculator.component.ts
+++ intro-app/src/app/reviewfolder/simplecalculator/simplecalculator.component.ts
@@ -9,6 +9,7 @@ export class SimplecalculatorComponent implements OnInit {
   operands: string[] = ['C', '+/-', '%', '/', 'x', '-', '+', '='];
   nums: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   value: number = 0;
+  previousValue: number = 0;
   chosenOperand: string = null;
   chosenNum: number = null;
   constructor() { }
@@ -17,16 +18,13 @@ export class SimplecalculatorComponent implements OnInit {
   }
 
   setNum(number: number){
-    this.chosenNum = number;
-    if(this.chosenNum && this.chosenOperand){
-      this.calculate(number);
-    }
-    else{
-      this.value = number;
-    }
+    let newValue: string = this.value.toString() + number;
+    //mapping out logic here since not using input field and forms module.
+    
   }
 
   setOperand(operand: string){
+    this.chosenNum = null;
     switch(operand){
       case 'C':
         this.value = 0;
@@ -43,14 +41,34 @@ export class SimplecalculatorComponent implements OnInit {
           this.value = this.value / 100;
         }
         break;
+      case '=':
+        this.calculate();
+        break;
       default:
         this.chosenOperand = operand;
         break;
     }
   }
 
-  calculate(num: number){
-    
+  calculate(){
+    if(this.chosenNum && this.previousValue && this.chosenOperand){
+      switch(this.chosenOperand){
+        case '+':
+          this.value = this.previousValue + this.chosenNum;
+          break;
+        case '-':
+          this.value = this.previousValue - this.chosenNum;
+          break;
+        case '/':
+          this.value = this.previousValue / this.chosenNum;
+          break;
+        case 'x':
+          this.value = this.previousValue * this.chosenNum;
+          break;
+        default:
+          return;
+      }
+    }
   }
 
 }
